e0c8f34df21f311e25f95b2a0569fdd9018c8dd7
NickyThreeNames <nich.cm@gmail.com>
2014-06-07 18:03:29 -0500

    Shiny Stuff

---
+++ ShinyInput/ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Illustrating inputs"),
+  sidebarPanel(
+    numericInput('id1','Numeric input, labeled id1', 0, min = 0, max=10, step = 1),
+    checkboxGroupInput("id2", "Checkbox",
+                       c("Value 1" = "1",
+                         "Value 2" = "2",
+                         "Value 3" = "3")),
+    dateInput("date", "Date:")
+    ),
+  mainPanel(
+    h3('Illustrating outputs'),
+    h3('You entered'),
+    verbatimTextOutput("oid1"),
+    h3('You entered'),
+    verbatimTextOutput("oid2"),
+    h3('You entered'),
+    verbatimTextOutput("odate"))
+  ))
