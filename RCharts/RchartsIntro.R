bad01585651d5367f8446fbeecad0b5d4fe2f376
NickyThreeNames <nich.cm@gmail.com>
2014-06-10 19:41:02 -0500

    latest updates

---
+++ RCharts/RchartsIntro.R
@@ -0,0 +1,4 @@
+#Create javascript visualizations using R - no need to learn D3 or javascript
+#Additional docs and install available on Github via Ramnath
+#install_github('rcharts', 'ramnathv') using devtools 
+
