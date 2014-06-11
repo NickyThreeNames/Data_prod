e240577b4e7c81394e990a27f5c47c3a8de49dd4
NickyThreeNames <nich.cm@gmail.com>
2014-06-10 19:22:37 -0500

    continued udpates

---
+++ Working with Images/ui.R
@@ -0,0 +1,8 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Example Plot"),
+  sidebarPanel(
+    sliderInput('mu', 'Guess at the mean', value = 70, min = 62, max = 74, step = 0.05)),
+  mainPanel(
+    plotOutput('newHist')
+    )
+  ))
